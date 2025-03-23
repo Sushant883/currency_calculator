72316fde6b502204e58cc637f7e9c4d1863e2f23
Sushant883 <panditsushant11@gmail.com>
2025-03-23 13:12:55 +0530

    add menu bar code

--- script.js
+++ script.js
@@ -2,25 +2,15 @@ const menu = document.getElementById('menu_bar');
 const toggleButton = document.getElementById('menu_toggle');
 const closeButton = document.getElementById('close_menu');
 
+// menu ko show krne ke liyea
 toggleButton.addEventListener("click", () => {
     menu.classList.add("active");
 });
 
+// menu ko remove krne ke liyea
 closeButton.addEventListener("click", () => {
     menu.classList.remove("active");
 });
 
 
-// const menu = document.getElementById("menu_bar");
-// const toggleButton = document.getElementById("menu_toggle");
-// const closeButton = document.getElementById("close_menu");
 
-// // Menu ko show karne ke liye
-// toggleButton.addEventListener("click", () => {
-//     menu.classList.add("active");
-// });
-
-// // Menu ko hide karne ke liye
-// closeButton.addEventListener("click", () => {
-//     menu.classList.remove("active");
-// });
