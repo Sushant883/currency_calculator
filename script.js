0ab7f062b207ac3f41ff1df3c371ba1cf150eeca
Sushant883 <panditsushant11@gmail.com>
2025-03-23 12:43:02 +0530

    add js code

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-const menu_bar = document.getElementById('menu_bar');
+const menu = document.getElementById('menu_bar');
+const toggleButton = document.getElementById('menu_toggle');
+const closeButton = document.getElementById('close_menu');
 
-
-menu_bar.addEventListener('click',() =>{ 
-
+toggleButton.addEventListener("click", () => {
+    menu.classList.add("active");
 });
+
+closeButton.addEventListener("click", () => {
+    menu.classList.remove("active");
+});
+
+
+// const menu = document.getElementById("menu_bar");
+// const toggleButton = document.getElementById("menu_toggle");
+// const closeButton = document.getElementById("close_menu");
+
+// // Menu ko show karne ke liye
+// toggleButton.addEventListener("click", () => {
+//     menu.classList.add("active");
+// });
+
+// // Menu ko hide karne ke liye
+// closeButton.addEventListener("click", () => {
+//     menu.classList.remove("active");
+// });
